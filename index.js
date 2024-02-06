a5d6eef8e963c040f9e968537f0a86e64ffedad4
jzmnmiller40 <ja154201@ucf.edu>
2024-02-05 18:13:57 -0600

    Created quote app
    Created quote app

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
-const generateQuote =function () {
-    const quotes = [
+let quotation =[
+"Well done is better than well said. - Benjamin Franklin",
+"Do not go where the path may lead, go instead where there is no path and leave a trail. -Ralph Waldo Emerson",
+"Be yourself;everyone else is already taken - Oscar Wilde",
+]
 
-    {
-        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-        author: "Nelson Mandela"
-    },
+// Get an interger between 0 and 2
+console.log(quotation[Math.floor(Math.random()*3)])
 
-    {
-        quote: "The way to get started is to quit talking and begin doing.",
-        author: "Walt Disney"
-    },
-
-    {
-        quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma which is living with the results of other people's thinking.",
-        author: "Steve Jobs"
-    },
-
-    {
-        quote: "If you dont take risks, you cant create a future!",
-        author: "Monkey D. Luffy (One Piece)"
-    },
-
-    {
-        quote: "Forgetting is like a wound. The wound may heal but it has already left a scar.",
-        author: "Monkey D Luffy (One Piece)"
-    },
-
-    {
-        quote: "The world isnt perfect But its there for us doing the best it can thats what makes it so damn beautiful.",
-
-        author: " Roy Mustang (Full Metal Alchemist)"
-    },
-
-
-    ];
-
-    let arrayIndex = Math.floor(Math.random() * quotes.length);
-
-    document.getElementById("quotes").innerHTML = quotes[arrayIndex].quote;
-    document.getElementById("author").innerHTML = quotes[arrayIndex].author;
-
-    }
-
-    window.onload = function() {
-        generateQuote();
-        document.getElementById("generate").addEventListener('click', generateQuote);
-    }
